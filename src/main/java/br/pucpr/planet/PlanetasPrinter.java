d0941b7bf35e63ba8798227fc3dcc62963f6a0f7
Giovani Nota Simões <giovani@simoes.pro>
2026-08-12 15:58:24 -0300

    feat: Add PlanetasPrinter w/ formatting

--- src/main/java/br/pucpr/planet/PlanetasPrinter.java
+++ src/main/java/br/pucpr/planet/PlanetasPrinter.java
@@ -7,10 +7,83 @@ import java.util.ArrayList;
 
 public class PlanetasPrinter {
   public void print(ArrayList<Planet> planetas, boolean alignRight, Theme theme) {
-    // Implemente sua solução aqui
+    if (planetas == null || planetas.isEmpty()) {
+      System.out.println("ERRO: Lista de planetas vazia ou nula.");
+      return;
+    }
+    final var borderChar = theme.getBorderChar();
+
+    // Borda superior e cabeçalho
+    final var BORDER_WIDTH = 70;
+    var sb = new StringBuilder();
+    sb.repeat(borderChar, BORDER_WIDTH).append("\n");
+    sb.append(String.format("| %-8s | %-10s | %-14s | %-14s | %-8s |%n", "Nome", "Diâmetro", "Dist. sol (km)", "Dist. sol (ua)", "Tipo"));
+    sb.repeat(borderChar, BORDER_WIDTH).append("\n");
+    for (var planeta : planetas) {
+      if (planeta == null) {
+        continue;
+      }
+
+      // 1 uma unidade astronomica (UA) é equivalente à distância da Terra ao Sol.
+      double sunDistanceUa = (double) planeta.sunDistanceKm() / 149_600_000L;
+
+      switch (planeta.type()){
+        case ROCK:
+          sb.append(
+                  String.format(
+                          "| %-8s | %-10s | %-14s | %-14s | %-8s |%n",
+                          planeta.name(),
+                          String.format("%,.1f", planeta.diameterKm()),
+                          String.format("%,d", planeta.sunDistanceKm()),
+                          String.format("%,.2f", sunDistanceUa),
+                          "Rochoso"));
+        case GAS:
+          sb.append(
+                  String.format(
+                          "| %-8s | %-10s | %-14s | %-14s | %-8s |%n",
+                          planeta.name(),
+                          String.format("%,.1f", planeta.diameterKm()),
+                          String.format("%,d", planeta.sunDistanceKm()),
+                          String.format("%,.2f", sunDistanceUa),
+                          "Gasoso")); // Prof você escrever gasoso errado no readme
+        case ICE:
+          sb.append(
+                  String.format(
+                          "| %-8s | %-10s | %-14s | %-14s | %-8s |%n",
+                          planeta.name(),
+                          String.format("%,.1f", planeta.diameterKm()),
+                          String.format("%,d", planeta.sunDistanceKm()),
+                          String.format("%,.2f", sunDistanceUa),
+                          "Gelado"));
+        case DWARF:
+          sb.append(
+                  String.format(
+                          "| %-8s | %-10s | %-14s | %-14s | %-8s |%n",
+                          planeta.name(),
+                          String.format("%,.1f", planeta.diameterKm()),
+                          String.format("%,d", planeta.sunDistanceKm()),
+                          String.format("%,.2f", sunDistanceUa),
+                          "Anão"));
+      }
+
+      // Borda inferior
+      sb.repeat(borderChar, BORDER_WIDTH).append("\n");
+
+      // Espaçamento
+      if (alignRight) {
+        var lines = sb.toString().split("\n");
+        for (var line : lines) {
+          System.out.println("                    " + line);
+        }
+      } else {
+        System.out.print(sb);
+      }
+    }
   }
 
-  static void main() {
+
+  // Eu coloquei o static e a ‘string’[] args para poder rodar pela IDE porque o javac não funcionava
+  public static void main(String[] args) {
     var planetas = new ArrayList<Planet>();
     planetas.add(new Planet("Mercúrio", 4879, 57_910_000L, ROCK));
     planetas.add(new Planet("Vênus", 12104, 108_200_000L, ROCK));
